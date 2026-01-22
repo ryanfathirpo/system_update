a41b3e5d57bc358231f6fc18fb2f2b9b7233f760
ryanfathirpo <rf13430916@gmail.com>
2026-01-23 00:17:43 +0800

    Fix dns_update EOL part with no space

--- sys_update.sh
+++ sys_update.sh
@@ -17,7 +17,7 @@ user=rfathi
 get_local_ips() {
   # Try ip command first (Linux)
   if command -v ip &>/dev/null; then
-    ip -4 addr show | grep -E 'inet ' | awk '{print $2}' | cut -d/ -f1 | grep  -e '10\.6\.53\.' -e '10\.7\.31\.' # If you want all the addresses use: grep -v '^127\.' anything but 127
+    ip -4 addr show | grep -E 'inet ' | awk '{print $2}' | cut -d/ -f1 | grep -e '10\.6\.53\.' -e '10\.7\.31\.' # If you want all the addresses use: grep -v '^127\.' anything but 127
   # Fallback to ifconfig (macOS/BSD)
   elif command -v ifconfig &>/dev/null; then
     ifconfig | grep -E 'inet ' | grep -E '10\.' | awk '{print $2}'
@@ -26,7 +26,6 @@ get_local_ips() {
     hostname -I 2>/dev/null || hostname -i 2>/dev/null || echo "Unable to determine IP"
   fi
 }
-
 
 basic_update() {
   if [[ $OSINSTALLER == 'apt' ]]; then
@@ -49,12 +48,12 @@ basic_update() {
   ZONE='rlab.lan'
   RECORD="$HOSTNAME.rlab.lan"
   cat <<EOL | nsupdate
-  server $DNS
-  zone $ZONE
-  update delete $RECORD A
-  update add $RECORD 3600 A $IP
-  send
-  EOL
+server $DNS
+zone $ZONE
+update delete $RECORD A
+update add $RECORD 3600 A $IP
+send
+EOL
 }
 vim_install() {
 
